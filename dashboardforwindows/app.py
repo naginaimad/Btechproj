8c9d8fe6983f1b20f65e32588dae031237d31afe
imad na <11bit012@nirmuani.ac.in>
2015-04-09 16:50:01 +0530

    windows compatible

---
+++ dashboardforwindows/app.py
@@ -0,0 +1,25 @@
+from flask import Flask,jsonify, render_template, request
+from flask.ext.pymongo import PyMongo
+import mimetypes
+
+
+
+app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/tweetDB2'
+mongo = PyMongo(app)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    tweets = mongo.db.RedMartcom.find({"classify": { "$exists":True}}).sort([("Created_at", -1)])
+    countcl = mongo.db.countclassify.find({"cneg": {  "$exists":True}})
+   
+    for RedMart in tweets,countcl:
+       
+        return render_template('index.html',tweets=tweets,countcl=countcl)
+
+if __name__ == '__main__':
+    mimetypes.add_type("text/css", ".css", True)
+    app.run(debug=True)
+
+
